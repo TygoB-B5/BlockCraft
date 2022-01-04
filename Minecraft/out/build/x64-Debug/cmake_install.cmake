875623a8f78d5f810dea9db4c811abb196070e26
Tygo <tygoboons@home.nl>
2022-01-04 23:49:27 +0100

    Added Basic Libs

--- Minecraft/out/build/x64-Debug/cmake_install.cmake
+++ Minecraft/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/tygo/Desktop/MinecraftCPP/Minecraft/Minecraft
+# Install script for directory: C:/Users/tygo/Desktop/MinecraftRemake/Minecraft
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/tygo/Desktop/MinecraftCPP/Minecraft/Minecraft/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/tygo/Desktop/MinecraftRemake/Minecraft/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,6 +32,14 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/tygo/Desktop/MinecraftRemake/Minecraft/out/build/x64-Debug/Vendor/glfw/cmake_install.cmake")
+  include("C:/Users/tygo/Desktop/MinecraftRemake/Minecraft/out/build/x64-Debug/Vendor/glad/cmake_install.cmake")
+  include("C:/Users/tygo/Desktop/MinecraftRemake/Minecraft/out/build/x64-Debug/Engine/Core/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -40,5 +48,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/tygo/Desktop/MinecraftCPP/Minecraft/Minecraft/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/tygo/Desktop/MinecraftRemake/Minecraft/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
