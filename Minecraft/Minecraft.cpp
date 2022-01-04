b78f34509cb2b6ab677b11979454ae501ac397e8
Tygo <tygoboons@home.nl>
2022-01-04 21:59:13 +0100

    Initial commit

---
+++ Minecraft/Minecraft.cpp
@@ -0,0 +1,12 @@
+﻿// Minecraft.cpp : Defines the entry point for the application.
+//
+
+#include "Minecraft.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
