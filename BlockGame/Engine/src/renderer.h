643fec823ddde02c44acea2e027837d21c435aaf
Tygo <tygoboons@home.nl>
2022-04-28 01:17:47 +0200

    Updated Renderer

--- BlockGame/Engine/src/renderer.h
+++ BlockGame/Engine/src/renderer.h
@@ -1,12 +1,42 @@
 #pragma once
 
+
+/*
+	Things I might wanna change:
+
+	Not sure if using a renderer object is neccesary it might as well all be static or Singleton.
+	Change to a more OOP approach.
+
+
+-----------------------------------------------------------------------
+	I have no idea how I want to do to abstract this renderer lmao.
+
+	Some ideas:
+
+	The draw process:
+	1. Set the vertex layout.
+	2. Gather all the required vertex data. (e.g. the sides of blocks with texcoords and albedo. Maybe also normals but I dont know what they would be useful for doee.)
+	3. Bind the neccesary shader.
+	4. Bind the neccesary textures. (using texture slots might be handy but I dont think it is neccesary if you use a texture atlas.)
+	5. Draw all elements
+
+	This requires some stuff tho:
+	Data container for Textures, Shaders, VAO, VBO, EBO
+	Abstraction for all of those things. (already did Textures and Shaders)
+
+	I also need to deal with the Input bs.
+
+*/
+
+
 #include "glad/glad.h"
 #include <glfw/glfw3.h>
 #include "glm/glm.hpp"
 #include <iostream>
 #include <assert.h>
+#include <array>
 #include <string>
-
+#include "stb_image.h"
 
 namespace engine
 {
@@ -57,30 +87,6 @@ namespace engine
 		GLFWwindow* _window;
 	};
 
-
-
-	class input
-	{
-	public:
-		bool isPressed(char key);
-		bool isReleased(char key);
-		bool isHeld(char key);
-
-	private:
-		window* _window;
-	};
-
-
-
-
-	enum shaderType
-	{
-		Vertex = 0,
-		Fragment = 1
-	};
-
-
-
 	struct rendererSettings
 	{
 		bool IsVsync = false;
@@ -94,9 +100,113 @@ namespace engine
 	public:
 
 		renderer(const window& window, const rendererSettings& settings)
-			: _isInitialized(false), _window(window), _renderSettings(settings), _shaderProgram(0)
+			: _isInitialized(false), _window(window), _renderSettings(settings), _shaderProgram(0), _activeTexture(0)
+		{}
+
+
+		class shader
 		{
-		}
+		public:
+
+			void bindShader()
+			{
+				assert(!(_id == 0), "Shader is not compiled.");
+				glUseProgram(_id);
+			}
+
+
+			void compileShader(const char* vertexSrc, const char* fragmentSrc)
+			{
+
+				// Create a vertex shader object
+				unsigned int vertexShader;
+				vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+
+				// Attach the shader source code to the shader object.
+				glShaderSource(vertexShader, 1, &vertexSrc, NULL);
+
+
+				// Compile the vertex shader dynamically.
+				glCompileShader(vertexShader);
+
+
+
+				// Check if compilation was successful.
+				GLint success = false;
+				char infoLog[512];
+
+				glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+				if (!success) {
+					glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+					std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n";
+					std::cout << infoLog << std::endl;
+				}
+
+
+				// Create a fragment shader object.
+				unsigned int fragmentShader;
+				fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+
+				// Attach the shader source code to the shader object.
+				glShaderSource(fragmentShader, 1, &fragmentSrc, NULL);
+
+
+				// Compile the fragment shader dynamically.
+				glCompileShader(fragmentShader);
+				glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+				if (!success) {
+					glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+					std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n";
+					std::cout << infoLog << std::endl;
+				}
+
+
+				// Create a shader program.
+				unsigned int shaderProgram;
+				shaderProgram = glCreateProgram();
+
+
+				// Attach the compiled shaders to the shader program.
+				glAttachShader(shaderProgram, vertexShader);
+				glAttachShader(shaderProgram, fragmentShader);
+				glLinkProgram(shaderProgram);
+				
+				// Check if linking was successful.
+				glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+				if (!success) {
+					glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+					std::cout << "ERROR::PROGRAM::LINKING_FAILED\n";
+					std::cout << infoLog << std::endl;
+				}
+
+
+				// Assign shaderProgram to renderer.
+				_id = shaderProgram;
+
+
+				// Delete shader objects if we no longer need them anymore.
+				glDeleteShader(vertexShader);
+				glDeleteShader(fragmentShader);
+
+			}
+
+
+		private:
+
+			uint32_t _id;
+		};
+
+		class texture
+		{
+			// TODO
+		};
+
+		class vertex
+		{
+
+		};
 
 
 		// Initialize the renderer
@@ -124,100 +234,86 @@ namespace engine
 
 		}
 
+		uint32_t glDatatypeSize(uint32_t type)
+		{
 
-		void setStride();
-		void setActiveTexture(uint32_t slot, void* data);
+			// Return size in bytes from Opengl datatype.
+			switch (type)
+			{
+			case GL_FLOAT:  return 4;
+			case GL_INT:    return 4;
+			case GL_DOUBLE: return 8;
+			default: assert(false, "Datatype not implemented.");
+			}
+
+		}
+
+		template<int S>
+		struct vertexLayout
+		{
+			std::array<uint32_t, S> Amount = { 0 };
+			std::array<uint32_t, S> DataType = { 0 };
+
+			uint32_t Stride = 0;
+		};
+
+		template <int S>
+		void setVertexLayout(const vertexLayout<S>& layout)
+		{
+			uint32_t offset = 0;
+
+			// Set stride and vertex layout.
+			for (size_t i = 0; i < S; i++)
+			{
+				glEnableVertexAttribArray(i);
+				glVertexAttribPointer(i, layout.Amount[i], layout.DataType[i], GL_FALSE, layout.Stride, (const void*)offset);
+
+				offset += glDatatypeSize(layout.DataType[i]) * layout.Amount[i];
+			}
+		}
+
+
+		void setTexture(uint32_t slot, const char* filename)
+		{	
+
+			// Load images.
+			int width, height, nrChannels;
+			stbi_set_flip_vertically_on_load(1);
+			unsigned char* data = stbi_load(filename, &width, &height, &nrChannels, 0);
+
+
+			// Create texture and assign data.
+			unsigned int texture;
+			glCreateTextures(GL_TEXTURE_2D, 1, &texture);
+			glBindTexture(GL_TEXTURE_2D, texture);
+
+			// Set texture filter settings.
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+
+
+			// Free image data.
+			stbi_image_free(data);
+
+			_activeTexture = texture;
+
+		}
+
+		void bindActiveTexture()
+		{
+			glBindTexture(GL_TEXTURE_2D, _activeTexture);
+		}
 
 		void addVertexElements();
 
-		void setShader(const char* vertexSrc, const char* fragmentSrc)
-		{
-			compileShader(vertexSrc, fragmentSrc);
-
-			_vertexShaderSource   = vertexSrc;
-			_fragmentShaderSource = fragmentSrc;
-		}
-
-
-
-		void compileShader(const char* vertexSrc, const char* fragmentSrc)
-		{
-			// Create a vertex shader object
-			unsigned int vertexShader;
-			vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-			// Attach the shader source code to the shader object
-			glShaderSource(vertexShader, 1, &vertexSrc, NULL);
-
-			// Compile the vertex shader dynamically 
-			glCompileShader(vertexShader);
-
-
-
-			// Check if compilation was successful
-			GLint success = false;
-			char infoLog[512];
-
-			glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-			if (!success) {
-				glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-				std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n";
-				std::cout << infoLog << std::endl;
-			}
-
-
-			// Create a fragment shader object
-			unsigned int fragmentShader;
-			fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-
-			// Attach the shader source code to the shader object
-			glShaderSource(fragmentShader, 1, &fragmentSrc, NULL);
-
-
-			// Compile the fragment shader dynamically
-			glCompileShader(fragmentShader);
-			glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-			if (!success) {
-				glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-				std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n";
-				std::cout << infoLog << std::endl;
-			}
-
-
-			// Create a shader program
-			unsigned int shaderProgram;
-			shaderProgram = glCreateProgram();
-
-
-			// Attach the compiled shaders to the shader program
-			glAttachShader(shaderProgram, vertexShader);
-			glAttachShader(shaderProgram, fragmentShader);
-			glLinkProgram(shaderProgram);
-
-			// Check if linking was successful
-			glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-			if (!success) {
-				glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-				std::cout << "ERROR::PROGRAM::LINKING_FAILED\n";
-				std::cout << infoLog << std::endl;
-			}
-
-
-			// Assign shaderProgram to renderer.
-			_shaderProgram = shaderProgram;
-
-
-			// Delete shader objects if we no longer need them anymore
-			glDeleteShader(vertexShader);
-			glDeleteShader(fragmentShader);
-
-		}
-
-
+		
 		// Clears the frame buffer
 		void clear()
 		{
+			assert(_isInitialized, "Renderer is not initialized.");
 
 			// Clear buffer data.
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -233,11 +329,14 @@ namespace engine
 
 		void draw()
 		{
+			assert(_isInitialized, "Renderer is not initialized.");
+
 			// Use saved shader
 			glUseProgram(_shaderProgram);
 
 			// Draw Triangles
-			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		}
 
 
@@ -248,6 +347,7 @@ namespace engine
 		rendererSettings _renderSettings;
 
 		uint32_t _shaderProgram;
+		uint32_t _activeTexture;
 
 		std::string _vertexShaderSource;
 		std::string _fragmentShaderSource;
--- BlockGame/game/src/entrypoint.cpp
+++ BlockGame/game/src/entrypoint.cpp
@@ -3,69 +3,115 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+const char* vertexShaderSource = R"(
 
-// Shader sources
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoord;
 
-const char* vertexShaderSource =
-"#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main() {\n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
+out vec2 uTexCoord;
+
+void main()
+{
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	uTexCoord = aTexCoord;
+}
+
+)";
 
 // Basic fragment shader
-const char* fragmentShaderSource =
-"#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main() {\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\0";
+const char* fragmentShaderSource = R"(
+
+#version 330 core
+out vec4 FragColor;
+
+in vec2 uTexCoord;
+uniform sampler2D uTexture;
+
+void main()
+{
+	FragColor = texture(uTexture, uTexCoord * 10);
+}
+
+)";
 
 
 
 
 int main()
 {
-	engine::renderer r(engine::window(100, 100, "window"), engine::rendererSettings());
+	engine::renderer r(engine::window(1000, 1000, "My window"), engine::rendererSettings());
 
-	float vertices[] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f,  0.5f, 0.0f
+	float vertices[4 * 5] = {
+	-0.5f, -0.5f, 0.0f,  0.0f, 0.0f,
+	 0.5f, -0.5f, 0.0f,  1.0f, 0.0f,
+	-0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 
+	 0.5f,  0.5f, 0.0f,  1.0f, 1.0f,
+	};
+
+	uint32_t indices[3 * 2]
+	{
+		0, 1, 2, 2, 1, 3
 	};
 
 
 	r.init();
 
 	// Generate vertex buffer object (VBO) and vertex array object (VAO)
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 
 	// Bind VAO, then bind VBO
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+
 	// Copy the vertex data into the buffer's memory
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Set attributes that describe how OpenGL should interpret the vertex data
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+
+
+	glBindVertexArray(VAO);
+
+	// TODO The stupid shader doesn't attach to the program or something.
+	engine::renderer::shader shader;
+	shader.compileShader(vertexShaderSource, fragmentShaderSource);
+	//shader.bindShader();
+
+	engine::renderer::vertexLayout<2> s;
+	s.Amount[0] = 3;
+	s.Amount[1] = 2;
+
+	s.DataType[0] = GL_FLOAT;
+	s.DataType[1] = GL_FLOAT;
+
+	s.Stride = 5 * sizeof(float);
+
+	r.setVertexLayout(s);
+
+	r.setTexture(0, "test.jpg");
 
 	// Unbind so other calls won't modify VBO and VAO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-
-	r.setShader(vertexShaderSource, fragmentShaderSource);
 
 	while (!glfwWindowShouldClose(r._window.get())) {
 
 		r.clear();
-		r.draw();
+
 
 		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		r.draw();
 
 		r._window.swapBuffer();
 		glfwPollEvents();
